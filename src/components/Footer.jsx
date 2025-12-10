445e2fa1510032c52f63c222aab5bceb2fd46847
sarmithaa28 <sarmithaaangamuthu@gmail.com>
2025-12-10 10:42:14 +0530

    Sarmii

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-6 mt-10">
-      <p>© 2025 LMS. All rights reserved.</p>
-    </footer>
-);
-}
